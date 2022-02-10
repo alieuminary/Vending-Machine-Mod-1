298ebb8b515e61013470eccbbb0500f0a4d7f48d
Andy Lieu <alieuedu@gmail.com>
2022-02-10 10:21:58 -0500

    added product.cs and display.cs

--- capstone/Capstone/Product.cs
+++ capstone/Capstone/Product.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace Capstone
 {
-    class Product
+    public class Product 
     {
+        public string Slot { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Type { get; set; }
+        public string Message { get; set; }
+        public string StockAmount { get; set; }
+        
+
+        public Product(string slot, string name, double price, string type) // to retrieve data from csv file
+        {
+            Slot = slot;
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+
+        // message method
+
+
+
+
+        
     }
 }
