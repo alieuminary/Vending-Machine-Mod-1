32d8c3712a43ec8092bdcf248ff4286dbe67c0da
Andy Lieu <alieuedu@gmail.com>
2022-02-11 16:38:11 -0500

    added VendingMachine.cs

---
+++ capstone/Capstone/VendingMachine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    class VendingMachine
+    {
+        // properties & attributes
+
+        public double Balance { get; set; }
+
+
+
+
+        // ========================= CONSTRUCTOR ================= //
+        //public VendingMachine(double balance)
+        //{
+        //    this.Balance = balance;
+        //}
+
+
+        // ========================= METHODS ===================== //
+
+        // customer inserts money into vending machine
+        public void FeedMoney(int amountOfMoney)
+        {
+            this.Balance += amountOfMoney;
+        }
+    }
+}
